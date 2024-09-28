bea002e587cceabeb0ce6a1eb196beb49ba15f78
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-28 14:51:52 +0530

    added get product by id endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,50 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/product/{id}", async (Guid id, ISender sender) =>
+        {
+            try
+            {
+                // mapster
+                var query = new GetProductByIdQuery
+                {
+                    Id = id
+                };
+
+                // mediatr
+                var result = await sender.Send(query);
+
+                // mapster
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+            }
+            catch (ProductNotFoundException ex)
+            {
+                return Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}
+
+//public record GetProductByIdRequest
+//{
+//}
+
+public record GetProductByIdResponse
+{
+    public Product Product { get; init; }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+internal class GetProductByIdQueryHandler : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    private readonly IDocumentSession session;
+    private readonly ILogger<GetProductByIdQueryHandler> logger;
+
+    public GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    {
+        this.session = session;
+        this.logger = logger;
+    }
+
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+        var product = await session
+            .LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        var result = new GetProductByIdResult
+        {
+            Product = product
+        };
+
+        return result;
+    }
+}
+
+public record GetProductByIdQuery : IQuery<GetProductByIdResult>
+{
+    public Guid Id { get; init; }
+}
+
+public record GetProductByIdResult
+{
+    public Product Product { get; init; }
+}
