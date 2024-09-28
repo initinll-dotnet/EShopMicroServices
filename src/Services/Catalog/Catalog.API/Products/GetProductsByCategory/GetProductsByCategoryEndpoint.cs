cf183cc1fd9cd0fae93905eb025562e52e5867a5
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-28 15:13:22 +0530

    added GetProductsByCategory endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,50 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            try
+            {
+                // mapster
+                var query = new GetProductsByCategoryQuery
+                {
+                    Category = category
+                };
+
+                // mediatr
+                var result = await sender.Send(query);
+
+                // mapster
+                var response = result.Adapt<GetProductsByCategoryResponse>();
+
+                return Results.Ok(response);
+            }
+            catch (ProductNotFoundException ex)
+            {
+                return Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        })
+        .WithName("GetProductsByCategory")
+        .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products By Category")
+        .WithDescription("Get Products By Category");
+    }
+}
+
+//public record GetProductsByCategoryRequest
+//{
+//}
+
+public record GetProductsByCategoryResponse
+{
+    public IEnumerable<Product> Products { get; init; }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,45 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+internal class GetProductsByCategoryQueryHandler : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    private readonly IDocumentSession session;
+    private readonly ILogger<GetProductsByCategoryQueryHandler> logger;
+
+    public GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+    {
+        this.session = session;
+        this.logger = logger;
+    }
+
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session
+            .Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        if (products is null)
+        {
+            Results.NotFound();
+        }
+
+        var result = new GetProductsByCategoryResult
+        {
+            Products = products
+        };
+
+        return result;
+    }
+}
+
+public record GetProductsByCategoryQuery : IQuery<GetProductsByCategoryResult>
+{
+    public string Category { get; init; }
+}
+
+public record GetProductsByCategoryResult
+{
+    public IEnumerable<Product> Products { get; init; }
+}
