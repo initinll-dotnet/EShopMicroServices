76c33e5ac718f0e62b4915b5cb7a884071b1e5ad
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-28 15:59:32 +0530

    added UpdateProductEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,50 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, CancellationToken token, ISender sender) =>
+        {
+            try
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+
+                var result = await sender.Send(command, token);
+
+                var response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            }
+            catch (ProductNotFoundException ex)
+            {
+                return Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
+
+public record UpdateProductRequest
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public List<string> Category { get; init; }
+    public string Description { get; init; }
+    public string ImageFile { get; init; }
+    public decimal Price { get; init; }
+}
+
+public record UpdateProductResponse
+{
+    public bool IsSuccess { get; init; }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+internal class UpdateProductCommandHandler : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    private readonly IDocumentSession session;
+    private readonly ILogger<UpdateProductCommandHandler> logger;
+
+    public UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    {
+        this.session = session;
+        this.logger = logger;
+    }
+
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.Category = command.Category;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        var result = new UpdateProductResult
+        {
+            IsSuccess = true
+        };
+
+        return result;
+    }
+}
+
+public record UpdateProductCommand : ICommand<UpdateProductResult>
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public List<string> Category { get; init; }
+    public string Description { get; init; }
+    public string ImageFile { get; init; }
+    public decimal Price { get; init; }
+}
+
+public record UpdateProductResult
+{
+    public bool IsSuccess { get; init; }
+}
+
+
