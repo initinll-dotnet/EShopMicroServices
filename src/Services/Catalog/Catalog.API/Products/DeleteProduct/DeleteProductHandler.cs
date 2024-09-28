cbc94d368b20786b8c61ace010b3209b9cc28c8c
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-28 16:19:49 +0530

    added DeleteProductEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+internal class DeleteProductCommandHandler : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    private readonly IDocumentSession session;
+    private readonly ILogger<DeleteProductCommandHandler> logger;
+
+    public DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    {
+        this.session = session;
+        this.logger = logger;
+    }
+
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        var result = new DeleteProductResult
+        {
+            IsSuccess = true
+        };
+
+        return result;
+    }
+}
+
+public record DeleteProductCommand : ICommand<DeleteProductResult>
+{
+    public Guid Id { get; init; }
+}
+
+public record DeleteProductResult
+{
+    public bool IsSuccess { get; init; }
+}
