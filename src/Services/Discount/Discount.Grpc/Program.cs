0e6ecbf0297d2b3744f5854b9d95f78229b06477
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-29 23:51:42 +0530

    added grpc health check

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -2,6 +2,7 @@ using Discount.Grpc.Data;
 using Discount.Grpc.Services;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ var connectionString = builder.Configuration.GetConnectionString("Database")!;
 // ########### Add services to the container ###########
 
 builder.Services.AddGrpc();
+builder.Services
+    .AddGrpcHealthChecks()
+    .AddCheck("health", () => HealthCheckResult.Healthy());
+
 builder.Services.AddDbContext<DiscountContext>(options =>
 {
     options.UseSqlite(connectionString);
@@ -26,6 +31,7 @@ if (builder.Environment.IsDevelopment())
     app.UseMigration();
 }
 app.MapGrpcService<DiscountService>();
+app.MapGrpcHealthChecksService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 // ########### Configure HTTP request pipeline ###########
