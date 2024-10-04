ad21e6e508694636823314ffb842d17325944df9
Nitin Londhe <nitin.londhe@genmills.com>
2024-10-04 20:25:38 +0530

    completed ordering.application layer with CQRS and Mediatr

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderUpdatedEventHandler : INotificationHandler<OrderUpdatedEvent>
+{
+    private readonly ILogger<OrderUpdatedEventHandler> logger;
+
+    public OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    {
+        this.logger = logger;
+    }
+
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
