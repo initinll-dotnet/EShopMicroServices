7f1aa4a3f6ab494183fbee7adedc3f13c93be73a
initinll <initinli@yahoo.com>
2024-11-10 00:29:33 +0530

    added web project

---
+++ src/WebApps/HealthCheck.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HealthCheck.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
